9bb7edf5a3e4624b490a63288d3db2811dbb6bd1
Jakob Runge <sicarius@g4t3.de>
2013-09-30 14:30:02 +0200

    Work in progress building the slides.
    - Next thing will be adding examples
    - Afterwards intro to raphael.js will follow

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+  //Building the paper:
+  var w = $(window).width()
+    , h = $(window).height()
+    , paper = Raphael(10, 10, w-20, h-20);
+  //Finding code blocks:
+  $('code[data-paper]').each(function(){
+    var t = '<pre class="language-javascript">'
+          + '<code data-trim contenteditable>'
+          + $(this).text().trim()
+          + '</code></pre>'
+          + '<a class="runLink">Run!</a>';
+    $(this).html(t);
+    hljs.highlightBlock($(this).find('pre code').get(0));
+  });
+  //Executing the code :)
+  $('a.runLink').click(function(e){
+    e.preventDefault();
+    var c = $(this).parent().find('code').text();
+    eval('window.callback = function(paper){' + c + '};');
+    paper.clear();
+    window.callback(paper);
+  });
+});
