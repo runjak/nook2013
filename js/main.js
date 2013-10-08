e9b7ed480f539baac27c752b1328379514f11ee8
Jakob Runge <sicarius@g4t3.de>
2013-10-08 07:10:34 +0200

    Dynamic insertion/removal of iframes for better performance.
    - Also building the finishing slides

--- js/main.js
+++ js/main.js
@@ -21,8 +21,44 @@ $(function(){
     paper.clear();
     window.callback(paper);
   });
-  //Cleaning the paper on slide change:
+  //Actions on slide change:
   Reveal.addEventListener('slidechanged', function(){
+    //Cleanup
     paper.clear();
+    $('iframe').remove();
+    //New iframes?
+    var t = $('section.present[data-iframe]');
+    if(t.length > 0){
+      var i = t.data('iframe');
+      t.prepend('<iframe scrolling="no" src="'+i+'"></iframe>');
+      i = t.find('iframe');
+      t.css('top', i.height()/-2 - 50 + 'px');
+    }
   });
+  //Unicorn magic!
+  var unicorn = {timeout: null, set: null};
+//Reveal.addEventListener('done', function(){
+    unicorn.timeout = window.setTimeout(function(){
+      /*
+        Unicorn built using inkscape after the preimage
+        from http://www.geekytattoos.com/does-she-dream-of-electric-sheep-too/
+        simplified by hand for alottafun…
+      */
+      var uPath = 'M6,0L8,14L7,15L3,21L1,19L0,20L2,23L5,24L8,19L9,20L11,18L13,20L14,19L20,19L26,11L22,13L24,4L16,13L14,12Z'
+                + 'M0,26L2,23L3,21'
+                + 'M7,15L11,18L12,11'
+                + 'M8,14L12,11L14,12'
+                + 'M11,18L14,14L13,20'
+                + 'M18,19L21,15L22,13'
+                + 'M16,13L20,14L14,19'
+                + 'M8,19L9,16.5'
+                + 'M16.25,17.25L13.75,15'
+                + 'M22.5,11L18.75,10';
+      unicorn.set = paper.path(uPath, 50, 50)
+           .attr({stroke: Raphael.getColor(), 'stroke-width': 2.5})
+           .transform('t200,200,s1,-1s8');
+      console.log('UNICORN!!!');
+    }, 1000);
+//});
+  window.unicorn = unicorn;
 });
